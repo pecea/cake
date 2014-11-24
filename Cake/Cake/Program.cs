463a4436f314b048eea97b6e3e2442078321a6a1
pecea <szyperski.p@gmail.com>
2014-11-24 21:08:21 +0100

    Added an empty project.

---
+++ Cake/Cake/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Cake
+{   
+    /// <summary>
+    /// Provides an entry point for the application.
+    /// </summary>
+    static class Program
+    {
+        /// <summary>
+        /// Entry point of the application.
+        /// </summary>
+        /// <param name="args">Paths to *.csx scripts to be executed.</param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
