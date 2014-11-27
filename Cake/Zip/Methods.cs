ae23e18ce0f4e060579a529d4edb8badb5a3e1a4
Ernest <ernestprzestrzelski@gmail.com>
2014-11-27 05:51:42 +0100

    Zipping functionality added

---
+++ Cake/Zip/Methods.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using Ionic.Zip;
+using Common;
+
+namespace Zip
+{
+    public static class Methods
+    {
+        #region methods
+
+        /// <summary>
+        /// Encloses methods used with zipping projects.
+        /// </summary>
+        /// <param name="zipName">Name of the zip file</param>
+        /// <param name="filePaths">Paths to files</param>
+        /// <param name="zipPath">Path of the zip to save</param>
+        /// <returns>true in case of success, false otherwise.</returns>
+        public static bool ZipFiles(string zipName, string[] filePaths, string zipPath = null)
+        {
+            CheckZipFilesArguments(filePaths, zipPath);
+            try
+            {
+                using (var zip = new ZipFile())
+                {
+                    zip.AddFiles(filePaths);
+                    zip.Save(!String.IsNullOrEmpty(zipPath)
+                        ? String.Format("{0}/{1}.zip", zipPath, zipName)
+                        : string.Format("{0}.zip", zipName));
+                    Logger.Log(LogLevel.Info, "{0} zipped succesfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(LogLevel.Error, ex, String.Format("Zipping {0}.zip failed", zipName));
+                return false;
+            }
+            return true;
+        }
+        /// <summary>
+        /// Checks if arguments passed to ZipFiles methods are valid.
+        /// </summary>
+        /// <param name="filePaths">Paths to files</param>
+        /// <param name="zipPath">Path of the zip to save</param>
+        /// <returns></returns>
+        private static bool CheckZipFilesArguments(IEnumerable<string> filePaths, string zipPath)
+        {
+            if (filePaths.Any(filePath => !File.Exists(filePath)))
+                return false;
+            try
+            {
+                Path.GetFullPath(zipPath);
+            }
+            catch (Exception)
+            {
+                Logger.Log(LogLevel.Error, "The zipPath parameter is not a valid path.");
+                return false;
+            }
+            return true;
+        }
+
+        #endregion
+    }
+}
