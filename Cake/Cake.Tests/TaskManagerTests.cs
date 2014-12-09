6592687f6b73c89b98f651f4b65d6c7855bbb2fc
Piotr Szyperski <szyperski.p@gmail.com>
2014-12-09 17:56:26 +0100

    Task status changed to prevent stack overflow when a circular dependency is defined.

--- Cake/Cake.Tests/TaskManagerTests.cs
+++ Cake/Cake.Tests/TaskManagerTests.cs
@@ -1,14 +1,15 @@
 ﻿namespace Cake.Tests
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+
     /// <summary>
-    /// Class for testing Task Manager methods
+    /// Contains tests concerning Task class/
     /// </summary>
     [TestClass]
     public class TaskManagerTests
     {
         /// <summary>
-        /// Test method for throwing an exception when dependency is not found
+        /// Tests whether an exception is thrown when a task is dependent on a non existing one.
         /// </summary>
         [TestMethod]
         [ExpectedException(typeof(TaskException), "A dependency on a non existing task was specified.")]
@@ -17,14 +18,28 @@
             new Task("test task").DependsOn("non existing task");
             TaskManager.RunTask("test task");
         }
+
         /// <summary>
-        /// Test method for throwing an exception when task is not found
+        /// Tests whether an exception is thrown when a non existing task is run.
         /// </summary>
         [TestMethod]
         [ExpectedException(typeof(TaskException), "Running a non existing task was ordered.")]
         public void RunTaskShouldThrowWhenTaskIsNotFound()
         {
             TaskManager.RunTask("non existing task");
+        }
+
+        /// <summary>
+        /// Tests whether an exception is thrown when a circular dependency is defined.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(TaskException), "Running a task with a circular dependency was orderd.")]
+        public void RunTaskShouldThrowWhenTheresDependencyCycle()
+        {
+            new Task("first").DependsOn("second");
+            new Task("second").DependsOn("first");
+
+            TaskManager.RunTask("first");
         }
     }
 }
---
+++ Cake/Cake/TaskStatus.cs
@@ -0,0 +1,21 @@
+﻿namespace Cake
+{
+    /// <summary>
+    /// Represents current status of a task.
+    /// </summary>
+    public enum TaskStatus
+    {
+        /// <summary>
+        /// Represents a task that has not been visited yet.
+        /// </summary>
+        NotVisited,
+        /// <summary>
+        /// Represents a task whose dependencies are being examined and executed.
+        /// </summary>
+        Pending,
+        /// <summary>
+        /// Represents a task whose action has been executed.
+        /// </summary>
+        Done
+    }
+}
