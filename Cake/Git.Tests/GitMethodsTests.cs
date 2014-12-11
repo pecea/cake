b4cbd8ccba7bdaa8b7592290939898c2880c7b5a
Ernest <ernestprzestrzelski@gmail.com>
2014-12-11 11:00:29 +0100

    Git unit tests

--- Cake/Git.Tests/GitMethodsTests.cs
+++ Cake/Git.Tests/GitMethodsTests.cs
@@ -9,6 +9,34 @@ namespace Git.Tests
         [TestMethod]
         public void TagShouldReturnSuccess()
         {
+            Methods.PathToExe = @"C:\Users\ernes_000\Source\Repos\Praca Inżynierska\Cake\Git.Tests\bin\Debug\testForCake";
+            Assert.AreEqual(true, Methods.Tag("unitTestTag"));
+            Assert.AreEqual(true, Methods.Tag("-d unitTestTag"));
+        }
+
+        [TestMethod]
+        public void ResetAllModificationsShouldReturnSucces()
+        {
+            Assert.AreEqual(true, Methods.ResetAllModifications());
+        }
+
+        [TestMethod]
+        public void PushShouldReturnSuccess()
+        {
+            Assert.AreEqual(true, Methods.Push(@"C:\Users\ernes_000\Source\Repos\Praca Inżynierska\Cake\Git.Tests\bin\Debug\testForCake"));
+        }
+
+        [TestMethod]
+        public void CleanShouldReturnSuccess()
+        {
+            Assert.AreEqual(true, Methods.Clean());
+        }
+
+        [TestMethod]
+        public void RunShouldReturnSuccess()
+        {
+            Assert.AreEqual(true, Methods.Run("tag unitTestTag"));
+            Assert.AreEqual(true, Methods.Run("tag -d unitTestTag"));
         }
     }
 }
