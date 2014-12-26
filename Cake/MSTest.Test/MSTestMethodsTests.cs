1684378b003649a1fa4d16680b9629a1b0e5de18
Ernest <ernestprzestrzelski@gmail.com>
2014-12-26 15:44:08 +0100

    Test for content comparison for zip method added and mstest tests changed (solution build in vs 13 errors corrected)

--- Cake/MSTest.Test/MSTestMethodsTests.cs
+++ Cake/MSTest.Test/MSTestMethodsTests.cs
@@ -1,28 +1,26 @@
 ﻿namespace MSTest.Test
 {
-    using Methods;
-
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
-    public class MSTestMethodsTests
+    public class MsTestMethodsTests
     {
         [TestMethod]
         public void ShouldReturnFailureIfInvalidTestPathIsSpecified()
         {
-            Assert.AreEqual(false, Test(@"X:\Invalid\Path\To\tests.dll"));
+            Assert.AreEqual(false, Methods.Test(@"X:\Invalid\Path\To\tests.dll"));
         }
 
         [TestMethod]
         public void ShouldReturnSuccessIfValidTestPathIsSpecified()
         {
-            Assert.AreEqual(true, Test(@"../../../Cake.Tests/bin/Debug/Cake.Tests.dll"));
+            Assert.AreEqual(true, Methods.Test(@"../../../Cake.Tests/bin/Debug/Cake.Tests.dll"));
         }
 
         [TestMethod]
         public void ShouldReturnFailureIfNotPassingTests()
         {
-            Assert.AreEqual(false, Test(@"../../../Build.Tests/bin/Debug/Build.Tests.dll"));
+            Assert.AreEqual(false, Methods.Test(@"../../../Build.Tests/bin/Debug/Build.Tests.dll"));
         }
     }
 }
--- Cake/Zip.Tests/ZipMethodsTests.cs
+++ Cake/Zip.Tests/ZipMethodsTests.cs
@@ -52,5 +52,39 @@ namespace Zip.Tests
             Assert.AreEqual(false, Methods.ZipFiles("exception", new[] { "testZipFile.txt" }, "invalid zip path"));
         }
         //TODO: przy ...ShouldReturnSuccess... zrobić dodatkowe testy sprawdzające identyczność plików przed i po zipowaniu
+        /// <summary>
+        /// Test method for successful comparison of zipped and unzipped files with not zipped files
+        /// </summary>
+        [TestMethod]
+        public void ZipFilesShouldReturnSuccessWhenZippedAndUnzippedFilesAreSameAsNotZippedFiles()
+        {
+            const string unzippedPath = "UnzippedFliesForTest";
+            var filesToTest = new[] {"testZipFile.txt", "testZipFile2.txt"};
+            if (Directory.Exists(unzippedPath))
+            {
+                var dir = new DirectoryInfo(unzippedPath);
+                foreach (var file in dir.GetFiles())
+                {
+                    file.Delete();
+                }
+                Directory.Delete(unzippedPath);
+            }
+            Methods.ZipFiles("sameContentTest", new[] {"testZipFile.txt", "testZipFile2.txt"});
+            System.IO.Compression.ZipFile.ExtractToDirectory("sameContentTest.zip", unzippedPath);
+            foreach (var file in filesToTest)
+            {
+
+                var fs = new FileStream(file, FileMode.Open, FileAccess.Read);
+                var fs2 = new FileStream(unzippedPath + "/" + file, FileMode.Open, FileAccess.Read);
+                int notZippedFileByte, unzippedFileByte;
+                Assert.AreEqual(fs.Length, fs2.Length);
+                do
+                {
+                    notZippedFileByte = fs.ReadByte();
+                    unzippedFileByte = fs2.ReadByte();
+                    Assert.AreEqual(notZippedFileByte, unzippedFileByte);
+                } while (notZippedFileByte != -1 && unzippedFileByte != -1);
+            }
+        }
     }
 }
