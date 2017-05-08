db1e2e727fe40becb0739f075eb8f50f90318c86
ernestprzestrzelski <ernestprzestrzelski@gmail.com>
2017-05-08 17:33:20 +0200

    test

--- Cake/GitHub/Methods.cs
+++ Cake/GitHub/Methods.cs
@@ -1,132 +1,132 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Octokit;
-using System.IO;
-using Common;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Octokit;
+//using System.IO;
+//using Common;
 
-namespace GitHub
-{
-    /// <summary>
-    /// Encloses methods used with integration with github.
-    /// </summary>
-    public static class Methods
-    {
-        private static string _oauthToken;
-        private static string _releaseNotesFile;
-        private static string _repository;
-        private static string _tagName;
-
-
-        private static ReleaseAssetUpload BuildAssetUpload(UploadFile item)
-        {
-            return new ReleaseAssetUpload { ContentType = item.ContentType ?? "application/octet-stream", FileName = Path.GetFileName(item.Path), RawData = File.OpenRead(item.Path) };
-        }
+//namespace GitHub
+//{
+//    /// <summary>
+//    /// Encloses methods used with integration with github.
+//    /// </summary>
+//    public static class Methods
+//    {
+//        private static string _oauthToken;
+//        private static string _releaseNotesFile;
+//        private static string _repository;
+//        private static string _tagName;
 
 
-        private static NewRelease BuildReleaseData()
-        {
-            var update = new NewRelease(_tagName);
-            if (_releaseNotesFile != null)
-            {
-                update.Body = File.ReadAllText(_releaseNotesFile);
-            }
-            return update;
-        }
-
-        /// <summary>
-        /// Method for making a release to github.
-        /// </summary>
-        /// <param name="repository">URL to repository</param>
-        /// <param name="oauthToken">Github oauthToken</param>
-        /// <param name="tagName">Tag</param>
-        /// <param name="files">Files included into release</param>
-        /// <param name="releaseNotesFile">Path to file with notes for the release</param>
-        /// <returns></returns>
-        public static bool Release(string repository, string oauthToken, string tagName, UploadFile[] files, string releaseNotesFile = null)
-        {
-            _repository = repository;
-            _oauthToken = oauthToken;
-            _tagName = tagName;
-            _releaseNotesFile = releaseNotesFile;
-
-            try
-            {
-                var client = new GitHubClient(new ProductHeaderValue("GitTfsTasks"), CredentialStore).Release;
-                var result = client.Create(Owner, RepositoryName, BuildReleaseData()).Result;
-                IdRelease = result.Id;
-                if ((files != null) && (files.Length != 0))
-                {
-                    files = (UploadFile[])files.Where((f) => (f.ContentType != null) && File.Exists(f.Path));
-                    //var upAssets = new List<string>();
-                    //foreach (var file in files)
-                    //{
-                    //    try
-                    //    {
-                    //        upAssets.Add(Upload(client, result, file));
-                    //        Logger.Log(LogLevel.Debug, "File "+Path.GetFileName(file.Path)+"uploaded correctly");
-                    //    }
-                    //    catch (Exception)
-                    //    {
-                    //        Logger.Log(LogLevel.Warn, "File "+Path.GetFileName(file.Path)+" not uploaded!");
-                    //    }
+//        private static ReleaseAssetUpload BuildAssetUpload(UploadFile item)
+//        {
+//            return new ReleaseAssetUpload { ContentType = item.ContentType ?? "application/octet-stream", FileName = Path.GetFileName(item.Path), RawData = File.OpenRead(item.Path) };
+//        }
 
 
-                    //}
-                    UploadedAssets = UploadAll(client, result, files);
-                    Logger.Log(LogLevel.Info, "Successful release!");
-                }
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
+//        private static NewRelease BuildReleaseData()
+//        {
+//            var update = new NewRelease(_tagName);
+//            if (_releaseNotesFile != null)
+//            {
+//                update.Body = File.ReadAllText(_releaseNotesFile);
+//            }
+//            return update;
+//        }
 
-        private static string TaskItemFor(Task<ReleaseAsset> asset)
-        {
-            return ("https://github.com/" + _repository + "/releases/download/" + _tagName + "/" + asset.Result.Name);
-        }
+//        /// <summary>
+//        /// Method for making a release to github.
+//        /// </summary>
+//        /// <param name="repository">URL to repository</param>
+//        /// <param name="oauthToken">Github oauthToken</param>
+//        /// <param name="tagName">Tag</param>
+//        /// <param name="files">Files included into release</param>
+//        /// <param name="releaseNotesFile">Path to file with notes for the release</param>
+//        /// <returns></returns>
+//        public static bool Release(string repository, string oauthToken, string tagName, UploadFile[] files, string releaseNotesFile = null)
+//        {
+//            _repository = repository;
+//            _oauthToken = oauthToken;
+//            _tagName = tagName;
+//            _releaseNotesFile = releaseNotesFile;
+
+//            try
+//            {
+//                var client = new GitHubClient(new ProductHeaderValue("GitTfsTasks"), CredentialStore).Release;
+//                var result = client.Create(Owner, RepositoryName, BuildReleaseData()).Result;
+//                IdRelease = result.Id;
+//                if ((files != null) && (files.Length != 0))
+//                {
+//                    files = (UploadFile[])files.Where((f) => (f.ContentType != null) && File.Exists(f.Path));
+//                    //var upAssets = new List<string>();
+//                    //foreach (var file in files)
+//                    //{
+//                    //    try
+//                    //    {
+//                    //        upAssets.Add(Upload(client, result, file));
+//                    //        Logger.Log(LogLevel.Debug, "File "+Path.GetFileName(file.Path)+"uploaded correctly");
+//                    //    }
+//                    //    catch (Exception)
+//                    //    {
+//                    //        Logger.Log(LogLevel.Warn, "File "+Path.GetFileName(file.Path)+" not uploaded!");
+//                    //    }
 
 
-        private static string Upload(IReleasesClient client, Release release, UploadFile sourceItem)
-        {
-            var asset = client.UploadAsset(release, BuildAssetUpload(sourceItem));
-            return TaskItemFor(asset);
-        }
+//                    //}
+//                    UploadedAssets = UploadAll(client, result, files);
+//                    Logger.Log(LogLevel.Info, "Successful release!");
+//                }
+//                return true;
+//            }
+//            catch (Exception)
+//            {
+//                return false;
+//            }
+//        }
+
+//        private static string TaskItemFor(Task<ReleaseAsset> asset)
+//        {
+//            return ("https://github.com/" + _repository + "/releases/download/" + _tagName + "/" + asset.Result.Name);
+//        }
 
 
-        private static string[] UploadAll(IReleasesClient client, Release release, IEnumerable<UploadFile> items)
-        {
-            return (from item in items select Upload(client, release, item)).ToArray();
-        }
+//        private static string Upload(IReleasesClient client, Release release, UploadFile sourceItem)
+//        {
+//            var asset = client.UploadAsset(release, BuildAssetUpload(sourceItem));
+//            return TaskItemFor(asset);
+//        }
+
+
+//        private static string[] UploadAll(IReleasesClient client, Release release, IEnumerable<UploadFile> items)
+//        {
+//            return (from item in items select Upload(client, release, item)).ToArray();
+//        }
         
-        private static ICredentialStore CredentialStore => new InPlaceCredentialStore(_oauthToken);
+//        private static ICredentialStore CredentialStore => new InPlaceCredentialStore(_oauthToken);
 
-        internal static int IdRelease { get; private set; }
+//        internal static int IdRelease { get; private set; }
         
-        private static string Owner => _repository.Split('/')[0];
+//        private static string Owner => _repository.Split('/')[0];
 
-        private static string RepositoryName => _repository.Split('/')[1];
+//        private static string RepositoryName => _repository.Split('/')[1];
 
-        internal static string[] UploadedAssets { get; private set; }
+//        internal static string[] UploadedAssets { get; private set; }
 
 
-        private class InPlaceCredentialStore : ICredentialStore
-        {
-            private static string _token;
+//        private class InPlaceCredentialStore : ICredentialStore
+//        {
+//            private static string _token;
             
-            public InPlaceCredentialStore(string token)
-            {
-                _token = token;
-            }
+//            public InPlaceCredentialStore(string token)
+//            {
+//                _token = token;
+//            }
 
-            public Task<Credentials> GetCredentials()
-            {
-                return Task.FromResult(new Credentials(_token));
-            }
-        }
-    }
-}
+//            public Task<Credentials> GetCredentials()
+//            {
+//                return Task.FromResult(new Credentials(_token));
+//            }
+//        }
+//    }
+//}
