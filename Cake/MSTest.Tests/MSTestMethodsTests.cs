cae3775010d666817eb62f9dd5063ef2c8069142
ernestprzestrzelski <ernestprzestrzelski@gmail.com>
2017-06-26 18:44:04 +0200

    most stuff

--- Cake/MSTest.Tests/MSTestMethodsTests.cs
+++ Cake/MSTest.Tests/MSTestMethodsTests.cs
@@ -1,29 +1,94 @@
 ﻿namespace MSTest.Tests
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Microsoft.VisualStudio.QualityTools;
 
     [TestClass]
     public class MsTestMethodsTests
     {
-        private const string PathToTestLibrary = @"../../Test Files/SampleUnitTestProject.dll";
-        private const string PathToFailingTestLibrary = @"../../Test Files/SampleFailingUnitTestProject.dll";
+        //private const string PathToTestLibrary = @"../../Test Files/PassingTests.dll";
+        private const string PathToTestLibrary = @"C:\Users\ErnestPrzestrzelski\Desktop\Ernest\priv\Backup\Praca Inzynierska\cake\Cake\MSTest.Tests\Test Files\PassingTests.dll";
+        //private const string PathToTestLibrary = @"PassingTests.dll";
+        private const string PathToFailingTestLibrary = @"../../Test Files/FailingTests.dll";
+        [TestMethod]
+        public void ShouldReturnSuccessIfValidTestPathIsSpecified()
+        {
+            Assert.IsTrue(Methods.RunTests(PathToTestLibrary));
+        }
 
         [TestMethod]
         public void ShouldReturnFailureIfInvalidTestPathIsSpecified()
         {
-            Assert.IsFalse(Methods.Test(@"X:\Invalid\Path\To\tests.dll"));
-        }
-
-        [TestMethod]
-        public void ShouldReturnSuccessIfValidTestPathIsSpecified()
-        {
-            Assert.IsTrue(Methods.Test(PathToTestLibrary));
+            Assert.IsFalse(Methods.RunTests(@"X:\Invalid\Path\To\tests.dll"));
         }
 
         [TestMethod]
         public void ShouldReturnFailureIfNotPassingTests()
         {
-            Assert.IsFalse(Methods.Test(PathToFailingTestLibrary));
+            Assert.IsFalse(Methods.RunTests(PathToFailingTestLibrary));
         }
+
+        [TestMethod]
+        public void ShouldRunOnlyFirstCategoryTests()
+        {
+            Assert.IsTrue(Methods.RunTests(PathToTestLibrary, "first"));
+
+        }
+
+        public void ShouldRunOnlySecondCategoryTests()
+        {
+            Assert.IsTrue(Methods.RunTests(PathToTestLibrary, "second"));
+        }
+
+        public void ShouldRunOnlyTestsThatHaveFirstAndSecondCategory()
+        {
+            Assert.IsTrue(Methods.RunTests(PathToTestLibrary, "first&second"));
+        }
+
+        public void ShouldRunTestsFromFirstOrSecondCategory()
+        {
+            Assert.IsTrue(Methods.RunTests(PathToTestLibrary, "first|second"));
+        }
+
+        public void ShouldRunOnlyTestsWithOnlyFirstCategory()
+        {
+            Methods.RunTests(PathToTestLibrary, "first&!second"); //first&!second?
+        }
+
+        public void ShouldNotRunAnyTests()
+        {
+            Methods.RunTests(PathToTestLibrary, "third");
+        }
+
+        public void ShouldSaveResultsToFile()
+        {
+            Methods.RunTests(PathToTestLibrary, null, "fileResult.trx");
+        }
+
+        public void ShouldSaveResultsToFileDifferentPath()
+        {
+            Methods.RunTests(PathToTestLibrary, null, "../../fileResult.trx");
+        }
+
+        public void ShouldRunOnlyOneTest()
+        {
+            Methods.RunTests(PathToTestLibrary, null, null, "TestMethod1");
+        }
+
+        public void ShouldRunSingleTests()
+        {
+            Methods.RunTests(PathToTestLibrary, null, "TestMethod1, TestMethod2");
+        }
+
+        public void ShouldThrowErrorNotUniqueSingleTestName()
+        {
+            Methods.RunTests(PathToTestLibrary, null, null, "TestMethod", true);
+        }
+
+        public void ShouldRunAllTests()
+        {
+            Methods.RunTests(PathToTestLibrary, null, null, "TestMethod");
+        }
+        //test single test + multiple single tests + uniqueness
     }
 }
