0f5ff1e6d0450919e44745090d71d80b00915ca1
Piotr Szyperski <szyperski.p@gmail.com>
2017-08-16 21:12:03 +0200

    Git tests

--- Cake/Git.Tests/GitMethodsTests.cs
+++ Cake/Git.Tests/GitMethodsTests.cs
@@ -1,85 +1,99 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using LibGit2Sharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace Git.Tests
 {
     [TestClass]
     public class GitMethodsTests
     {
-        const string Path = @"..\..\Test Files\UnitTestRepository\";
-        
-        //Commit
-        //Diff
-        //Fetch
-        //Pull
-        //Push
-        //Reset
-        //Stage
+        private static int CreatedFilesCount = 0;
+        private static Commit InitialCommit;
 
+        [ClassInitialize]
+        public static void CreateRepository(TestContext context)
+        {
+            Methods.RepositoryPath = Path.Combine(context.TestRunResultsDirectory, "TestRepo");
+            Methods.UserIdentity.Name = "Test User";
+            Methods.UserIdentity.Email = "testuser@example.com";
 
+            Repository.Init(Methods.RepositoryPath);
+            MakeInitialCommit();
+        }
 
-        //[TestInitialize]
-        //public void CreateRepository()
-        //{
-        //    //Directory.SetCurrentDirectory(Directory.GetCurrentDirectory());
-        //    //Methods.PathToRepository = Directory.GetCurrentDirectory();
-        //    if (!Directory.Exists(Path))
-        //        Directory.CreateDirectory(Path);
-        //    Directory.SetCurrentDirectory(Path);
-        //    Methods.Run("init");
-        //    Methods.Run("add");
-        //    //Methods.Run("commit");
-        //}
+        [TestInitialize]
+        public void ResetRepository()
+        {
+            using (var repo = new Repository(Methods.RepositoryPath))
+            {
+                repo.Reset(ResetMode.Hard, InitialCommit);
+                repo.RemoveUntrackedFiles();
+            }
+        }
 
-        //[TestMethod]
-        //public void TagShouldReturnSuccess()
-        //{
-        //    Directory.SetCurrentDirectory(Path);
-        //    Assert.IsTrue(Methods.Tag("unitTestTagut"));
-        //    Assert.IsTrue(Methods.Tag("-d unitTestTagut"));
-        //}
+        private static void MakeInitialCommit()
+        {
+            using (var repo = new Repository(Methods.RepositoryPath))
+            {
+                var signature = Methods.UserIdentity.GetSignature();
+                InitialCommit = repo.Commit("Test initial commit", signature, signature);
+            }
+        }
 
-        //[TestMethod]
-        //public void CleanShouldReturnSuccess()
-        //{
-        //    Directory.SetCurrentDirectory(Path);
-        //    Assert.IsTrue(Methods.Clean());
-        //}
+        [TestMethod]
+        public void AfterCommittingAllChangesShouldBeZeroWorkingDirChanges()
+        {
+            Assert.AreEqual(0, GetWorkingDirChangesCount());
 
-        //[TestMethod]
-        //public void RunShouldReturnSuccess()
-        //{
-        //    Directory.SetCurrentDirectory(Path);
-        //    Assert.IsTrue(Methods.Run("tag unitTestTagut"));
-        //    Assert.IsTrue(Methods.Run("tag -d unitTestTagut"));
-        //}
+            CreateFile();
+            Assert.AreEqual(1, GetWorkingDirChangesCount());
 
-        //[TestMethod]
-        //public void CurrentBranchShouldReturnSuccess()
-        //{
-        //    Directory.SetCurrentDirectory(Path);
-        //    Assert.IsTrue(Methods.CurrentBranch());
-        //}
+            Methods.CommitAllChanges("Test commit");
+            Assert.AreEqual(0, GetWorkingDirChangesCount());
+        }
 
-        //[TestMethod]
-        //public void CurrentShaShouldReturnSuccess()
-        //{
-        //    Directory.SetCurrentDirectory(Path);
-        //    Assert.IsTrue(Methods.CurrentSha());
-        //}
+        [TestMethod]
+        public void AfterStagingAFileShouldBeOneMoreIndexChange()
+        {
+            Assert.AreEqual(0, GetWorkingDirChangesCount());
+            string filePath = CreateFile();
+            Assert.AreEqual(1, GetWorkingDirChangesCount());
 
-        //[TestCleanup]
-        //public void CleanUp()
-        //{
-        //    try
-        //    {
-        //        Directory.SetCurrentDirectory(Path);
-        //        Methods.Clean(true);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Trace.WriteLine(ex);
-        //        throw;
-        //    }
-        //}
+            var indexChanges = GetIndexChangesCount();
+            Methods.Stage(filePath);
+            Assert.AreEqual(indexChanges + 1, GetIndexChangesCount());
+        }
+
+        [TestMethod]
+        public void AfterResetShouldBeZeroIndexChanges()
+        {
+            string filePath = CreateFile();
+
+            Assert.AreEqual(0, GetIndexChangesCount());
+            Methods.Stage(filePath);
+            Assert.AreEqual(1, GetIndexChangesCount());
+            Methods.ResetHard();
+            Assert.AreEqual(0, GetIndexChangesCount());
+        }
+
+        private int GetWorkingDirChangesCount() => GetChangesCount(DiffTargets.WorkingDirectory);
+
+        private int GetIndexChangesCount() => GetChangesCount(DiffTargets.Index);
+
+        private int GetChangesCount(DiffTargets diffTarget)
+        {
+            using (var repo = new Repository(Methods.RepositoryPath))
+            {
+                return repo.Diff.Compare<TreeChanges>(repo.Head.Tip.Tree, diffTarget).Count;
+            }
+        }
+
+        private string CreateFile()
+        {
+            string path = Path.Combine(Methods.RepositoryPath, $"test_file_{CreatedFilesCount++}.txt");
+            using (File.Create(path)) { }
+
+            return path;
+        }
     }
 }
