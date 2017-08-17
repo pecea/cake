5c318c11bd2048baaa8375a38b6c53883539d1be
ernestprzestrzelski <ernestprzestrzelski@gmail.com>
2017-08-17 17:41:13 +0200

    BuildResult

---
+++ Cake/Build/BuildResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Build
+{
+    internal class BuildResult
+    {
+        internal string OutputPath   { get; set;}
+        internal string PdbPath      { get; set;}
+        internal string XmlPath { get; set; }
+    }
+}
