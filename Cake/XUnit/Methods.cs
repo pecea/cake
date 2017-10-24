979a8b04f11d404bcf6d6da366b8a0adfd1abb62
Piotr Szyperski <piotr.szyperski@bankup.io>
2017-10-24 18:32:46 +0200

    Test methods modified

--- Cake/XUnit/Methods.cs
+++ Cake/XUnit/Methods.cs
@@ -26,34 +26,27 @@ namespace XUnit
                 Logger.Log(LogLevel.Warn, "xunit.console.exe file not found.");
                 return false;
             }
-            try
+
+            if (assemblyPaths.Any(ass => string.IsNullOrEmpty(ass) || !File.Exists(ass)))
             {
-                if (assemblyPaths.Any(ass => string.IsNullOrEmpty(ass) || !File.Exists(ass)))
-                {
-                    Logger.Log(LogLevel.Warn, "Incorrect test assemby paths!\n");
-                    return false;
-                }
-
-                var parameters = assemblyPaths.Aggregate((current, path) => current + $" {Processor.QuoteArgument(path)}");
-                if (!string.IsNullOrEmpty(traits))
-                    parameters = traits.Split(',').Select(t => t.Trim()).Aggregate(parameters, (current, trait) => current + $" -trait {Processor.QuoteArgument(trait)} ");
-                if (!string.IsNullOrEmpty(notraits))
-                    parameters = notraits.Split(',').Select(nt => nt.Trim()).Aggregate(parameters, (current, notrait) => current + $" -notrait {Processor.QuoteArgument(notrait)}");
-                parameters += " -nologo";
-
-                var result = Processor.RunProcess(FullPathExe, parameters);
-
-                if (!string.IsNullOrEmpty(result.Output))
-                    res = result.Output.Contains(TestsPassed);
-
-                Logger.LogMethodEnd();
-                return res;
-            }
-            catch (Exception e)
-            {
-                Logger.LogException(LogLevel.Error, e, "An exception occured while running xunit.console.exe.");
+                Logger.Log(LogLevel.Warn, "Incorrect test assemby paths!\n");
                 return false;
             }
+
+            var parameters = assemblyPaths.Aggregate((current, path) => current + $" {Processor.QuoteArgument(path)}");
+            if (!string.IsNullOrEmpty(traits))
+                parameters = traits.Split(',').Select(t => t.Trim()).Aggregate(parameters, (current, trait) => current + $" -trait {Processor.QuoteArgument(trait)} ");
+            if (!string.IsNullOrEmpty(notraits))
+                parameters = notraits.Split(',').Select(nt => nt.Trim()).Aggregate(parameters, (current, notrait) => current + $" -notrait {Processor.QuoteArgument(notrait)}");
+            parameters += " -nologo";
+
+            var result = Processor.RunProcess(FullPathExe, parameters);
+
+            if (!string.IsNullOrEmpty(result.Output))
+                res = result.Output.Contains(TestsPassed);
+
+            Logger.LogMethodEnd();
+            return res;
         }
         /// <summary>
         /// Runs XUnit tests from the specified assemblyPath with different options
@@ -71,7 +64,7 @@ namespace XUnit
         /// <param name="outputTypeAndName">Option to specify output type and filename. Possible values: xml filename - xUnit.net v2 style XML file, xmlv1 filename - xUnit.net v1 style XML file, nunit filename - NUnit-style XML file, html filename - HTML file</param>
         /// <returns>True if xunit tests run successfully, otherwise false</returns>
 
-        public static bool RunTestsWithOptions(string assemblyPaths, string traits = null, string notraits = null, string methodname = null, string classname = null, 
+        public static bool RunTestsWithOptions(string assemblyPaths, string traits = null, string notraits = null, string methodname = null, string classname = null,
             string parallel = null, int? maxthreads = null, bool? noshadow = null, bool? quiet = null, bool? serialize = null, string outputTypeAndName = null)
         {
             Logger.LogMethodStart();
@@ -81,51 +74,44 @@ namespace XUnit
                 Logger.Log(LogLevel.Warn, "xunit.console.exe file not found.");
                 return false;
             }
-            try
+
+            var paths = assemblyPaths.Split(',').Select(ass => ass.Trim()).ToArray();
+            if (paths.Any(ass => string.IsNullOrEmpty(ass) || !File.Exists(ass)))
             {
-                var paths = assemblyPaths.Split(',').Select(ass => ass.Trim()).ToArray();
-                if (paths.Any(ass => string.IsNullOrEmpty(ass) || !File.Exists(ass)))
-                {
-                    Logger.Log(LogLevel.Warn, "Incorrect test assemby paths!\n");
-                    return false;
-                }
-                var parameters = paths.Aggregate((current, path) => current + $" {Processor.QuoteArgument(path)}");
-
-                if (!string.IsNullOrEmpty(traits))
-                    parameters = traits.Split(',').Select(t => t.Trim()).Aggregate(parameters, (current, trait) => current + $" -trait {Processor.QuoteArgument(trait)}");
-                if (!string.IsNullOrEmpty(notraits))
-                    parameters = notraits.Split(',').Select(nt => nt.Trim()).Aggregate(parameters, (current, notrait) => current + $" -notrait {Processor.QuoteArgument(notrait)}");
-                if (!string.IsNullOrEmpty(methodname))
-                    parameters += $" -method {methodname}";
-                if (!string.IsNullOrEmpty(classname))
-                    parameters += $" -class {classname}";
-                if (!string.IsNullOrEmpty(parallel))
-                    parameters += $" -parallel {parallel}";
-                if (maxthreads != null)
-                    parameters += $" -maxthreads {maxthreads}";
-                if (noshadow != null && noshadow.Value)
-                    parameters += " -noshadow";
-                if (quiet != null && quiet.Value)
-                    parameters += " -quiet";
-                if (serialize != null && serialize.Value)
-                    parameters += " -serialize";
-                if (!string.IsNullOrEmpty(outputTypeAndName))
-                    parameters += $" -{outputTypeAndName}";
-                parameters += " -nologo";
-
-                var result = Processor.RunProcess(FullPathExe, parameters);
-
-                if (!string.IsNullOrEmpty(result.Output))
-                    res = result.Output.Contains(TestsPassed);
-
-                Logger.LogMethodEnd();
-                return res;
-            }
-            catch (Exception e)
-            {
-                Logger.LogException(LogLevel.Error, e, "An exception occured while running xunit.console.exe.");
+                Logger.Log(LogLevel.Warn, "Incorrect test assemby paths!\n");
                 return false;
             }
+            var parameters = paths.Aggregate((current, path) => current + $" {Processor.QuoteArgument(path)}");
+
+            if (!string.IsNullOrEmpty(traits))
+                parameters = traits.Split(',').Select(t => t.Trim()).Aggregate(parameters, (current, trait) => current + $" -trait {Processor.QuoteArgument(trait)}");
+            if (!string.IsNullOrEmpty(notraits))
+                parameters = notraits.Split(',').Select(nt => nt.Trim()).Aggregate(parameters, (current, notrait) => current + $" -notrait {Processor.QuoteArgument(notrait)}");
+            if (!string.IsNullOrEmpty(methodname))
+                parameters += $" -method {methodname}";
+            if (!string.IsNullOrEmpty(classname))
+                parameters += $" -class {classname}";
+            if (!string.IsNullOrEmpty(parallel))
+                parameters += $" -parallel {parallel}";
+            if (maxthreads != null)
+                parameters += $" -maxthreads {maxthreads}";
+            if (noshadow != null && noshadow.Value)
+                parameters += " -noshadow";
+            if (quiet != null && quiet.Value)
+                parameters += " -quiet";
+            if (serialize != null && serialize.Value)
+                parameters += " -serialize";
+            if (!string.IsNullOrEmpty(outputTypeAndName))
+                parameters += $" -{outputTypeAndName}";
+            parameters += " -nologo";
+
+            var result = Processor.RunProcess(FullPathExe, parameters);
+
+            if (!string.IsNullOrEmpty(result.Output))
+                res = result.Output.Contains(TestsPassed);
+
+            Logger.LogMethodEnd();
+            return res;
         }
     }
 }
