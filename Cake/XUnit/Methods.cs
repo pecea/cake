322842289c0af994b85007dd5ec7353a4762c9cd
ernestprzestrzelski <ernestprzestrzelski@gmail.com>
2017-10-26 19:02:13 +0200

    comment fix

--- Cake/XUnit/Methods.cs
+++ Cake/XUnit/Methods.cs
@@ -58,7 +58,7 @@ namespace XUnit
         /// <param name="notraits">Attributes on test methods must not be a dictionary in a form of names and values</param>
         /// <param name="methodname">Fully specified methodname of a test method - namespace, classname and methodname</param>
         /// <param name="classname">Fully specified classname of a test class - namespace and classname</param>
-        /// <param name="parallel">Sets parallelization. Possible values: none - turn off all parallelization, collections - only parallelize collections, assemblies - only parallelize assemblies, all - parallelize assemblies & collections</param>
+        /// <param name="parallel">Sets parallelization. Possible values: none - turn off all parallelization, collections - only parallelize collections, assemblies - only parallelize assemblies, all - parallelize assemblies and collections</param>
         /// <param name="maxthreads">Maximum thread count for collection parallelization. Possible values: 0 - run with unbounded thread count, >0 - limit task thread pool size to 'count'</param>
         /// <param name="noshadow">Flag indicating whether to shadow copy assemblies</param>
         /// <param name="quiet">Flag indicating whether to show progress messages</param>
